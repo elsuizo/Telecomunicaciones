62d1a9b64009d9c10a68ffb2fa255d294c3e7ce0
elsuizo <martin.noblia@openmailbox.org>
2015-04-30 14:02:14 -0300

    update

--- Laboratorios/Lab1/Programas/Julia/teleco.jl
+++ Laboratorios/Lab1/Programas/Julia/teleco.jl
@@ -1,16 +1,38 @@
-#! /usr/bin/env julia
+module Teleco
 
+type DiscretSource
+    source :: UTF8String
+    entropy :: Float64
+    code :: Dict
+    probs :: Vector
+end
+
+    
+    
 # Funcion para calcular las probabilidades de ocurrencia de cada simbolo existente en 
 # una fuente de informacion
 
-function probabilidad_txt(T::UTF8String)
+function probs_letters(ds::DiscretSource)
     # Compute the probabilities of letters
-    d = Dict{Char, FloatingPoint}()
-    prob = [length(find(T.data .== letter)) for letter in Set(T.data)] / (length(T.data))
-    d = {letter => i for i in prob, letter in Set(T)}
+    d = Dict{Char, Float64}()
+    prob = [length(find(ds.source.data .== letter)) for letter in Set(ds.source.data)] / (length(ds.source.data))
+    d = {letter => i for i in prob, letter in Set(ds.source.data)}
      
     return d,prob
 
 end
+
+function information(ds::DiscretSource)
+
+    i = log2(1 ./ ds.probs)
+    return i
+end
+
+#function entropia(d)
+export probs_letters, information, DiscretSource
+end    
+
+#export probabilidad_txt, informacion
+#end
 
 

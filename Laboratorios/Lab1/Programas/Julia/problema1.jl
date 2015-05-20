272179040f5fa5969bcace33e3b42f34e978d029
elsuizo <martin.noblia@openmailbox.org>
2015-05-19 23:56:44 -0300

    scripts resolucion de incisos

---
+++ Laboratorios/Lab1/Programas/Julia/problema1.jl
@@ -0,0 +1,17 @@
+#= Script para el problema 1
+desde consola:
+julia problema1.jl
+=#
+# incluimos el modulo
+include("teleco.jl")
+# leemos el archivo
+T = open(readall,"../Archivos/texto.txt");
+# Filtramos el texto para quitar los "\n" y los espacios " "
+T = filter!(x->(x!='\n')&&(x!=' '),collect(T));
+# calculamos las frecuencias y probabilidades 
+d,p = Teleco.probs(T);
+# Mostramos los resultados
+Teleco.show_source(d)
+# Calculamos la entropia de la fuente
+h = Teleco.entropy(p);
+print("La fuente discreta texto.txt tiene una entropia: $h [bits/simbolo]\n")
---
+++ Laboratorios/Lab1/Programas/Julia/problema3.jl
@@ -0,0 +1,50 @@
+# Script para la resolucion del problema 3
+
+# incluimos el modulo
+include("teleco.jl")
+
+# leemos los bytes de la fuente castellano
+s1 = open(readbytes,"../Archivos/castellano.txt");
+# leemos los bytes de la fuente ingles
+s2 = open(readbytes,"../Archivos/ingles.txt");
+# leemos los bytes de la fuente castellano zipeada
+s1_zip = open(readbytes,"../Archivos/castellano.txt.zip");
+# leemos los bytes de la fuente ingles zipeada
+s2_zip = open(readbytes,"../Archivos/ingles.txt.zip");
+
+# calculamos las longitudes de los archivos(bytes)
+l1 = length(s1);
+l2 = length(s2);
+l1_zip = length(s1_zip);
+l2_zip = length(s2_zip);
+
+# calculamos los ratios
+r1 = (l1_zip / l1) * 100;
+r2 = (l2_zip / l2) * 100;
+
+# mostramos los resultados
+print("La eficiencia de compresión para el caso de la fuente en castellano es: $r1\n")
+
+print("La eficiencia de compresión para el caso de la fuente en ingles es: $r2\n")
+
+# Leemos los archivos como texto plano
+Tc = open(readall,"../Archivos/castellano.txt");
+Ti = open(readall,"../Archivos/ingles.txt");
+
+# Filtramos el texto para quitar los "\n" y los espacios " "
+Tc = filter!(x->(x!='\n')&&(x!=' '),collect(Tc));
+Ti = filter!(x->(x!='\n')&&(x!=' '),collect(Ti));
+
+# calculamos las frecuencias y probabilidades 
+dc,pc = Teleco.probs(Tc);
+d_i,p_i= Teleco.probs(Ti);
+# Mostramos los resultados
+println("Las frecuencias para el texto en castellano son:\n\n")
+Teleco.show_source(dc)
+println("Las frecuencias para el texto en ingles son:\n\n")
+Teleco.show_source(d_i)
+hc = Teleco.entropy(pc);
+hi = Teleco.entropy(p_i);
+
+print("La fuente discreta castellano.txt tiene una entropia: $hc [bits/simbolo]\n")
+print("La fuente discreta ingles.txt tiene una entropia: $hi [bits/simbolo]\n")

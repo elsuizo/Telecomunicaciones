c602c6bc0f9eaa203d2e5fa0550601dd7289b8f7
elsuizo <martin.noblia@openmailbox.org>
2015-05-21 23:31:26 -0300

    update

--- Laboratorios/Lab1/Programas/Julia/problema6.jl
+++ Laboratorios/Lab1/Programas/Julia/problema6.jl
@@ -1,4 +1,4 @@
-# script problema 5
+# Script para resolver el problema 6
 
 #-------------------------------------------------------------------------
 # Includes 
@@ -9,14 +9,14 @@ include("teleco2.jl")
 
 # leemos el archivo de voz
 x,fs = wavread("audio_voz.wav")
-# cuantizamos uniformente con 4 bits
-x_q₄ = Teleco2.unifor_quatization(x,16) 
-x_q₃ = Teleco2.unifor_quatization(x,9) 
-x_q₂ = Teleco2.unifor_quatization(x,4) 
+# cuantizamos con la ley-mu 4 bits y mu=255=0xff
+x_q₄ = Teleco2.μ_quantization(x,16,255) 
+x_q₃ = Teleco2.μ_quantization(x,9,255) 
+x_q₂ = Teleco2.μ_quantization(x,4,255) 
 
-wavwrite(x_q₄,"audio_voz_q_4.wav")
-wavwrite(x_q₃,"audio_voz_q_3.wav")
-wavwrite(x_q₂,"audio_voz_q_2.wav")
+wavwrite(x_q₄,"audio_voz_q_4_mu.wav")
+wavwrite(x_q₃,"audio_voz_q_3_mu.wav")
+wavwrite(x_q₂,"audio_voz_q_2_mu.wav")
 
 xₑ₂ = x-x_q₂
 xₑ₄ = x-x_q₄
@@ -27,9 +27,9 @@ s_n₂ = 10*log10((mean(x_q₂ .* x_q₂)) / (mean(xₑ₂ .* xₑ₂)))
 s_n₃ = 10*log10((mean(x_q₃ .* x_q₃)) / (mean(xₑ₃ .* xₑ₃)))
 s_n₄ = 10*log10((mean(x_q₄ .* x_q₄)) / (mean(xₑ₃ .* xₑ₄)))
 
-println("La relación senial-ruido para 2 bits es: $s_n₂[Db]")
-println("La relación senial-ruido para 3 bits es: $s_n₃[Db]")
-println("La relación senial-ruido para 4 bits es: $s_n₄[Db]")
+println("La relación senial-ruido para 2 bits es: $s_n₂ [Db]")
+println("La relación senial-ruido para 3 bits es: $s_n₃ [Db]")
+println("La relación senial-ruido para 4 bits es: $s_n₄ [Db]")
 
 fig = figure(figsize=(10,10)) 
 ax = gca()
@@ -41,7 +41,7 @@ axis("tight")
 title("Comparacion de seniales")
 grid("on")
 legend(loc="upper right",fancybox="true") 
-savefig("senialesv_voz_4_bits.eps")
+savefig("senialesv_voz_4_bits_mu.eps")
 
 fig = figure(figsize=(10,10)) 
 ax = gca()
@@ -53,7 +53,7 @@ axis("tight")
 title("Comparacion de seniales")
 grid("on")
 legend(loc="upper right",fancybox="true") 
-savefig("senialesv_voz_3_bits.eps")
+savefig("senialesv_voz_3_bits_mu.eps")
 
 fig = figure(figsize=(10,10)) 
 ax = gca()
@@ -65,5 +65,5 @@ axis("tight")
 title("Comparacion de seniales")
 grid("on")
 legend(loc="upper right",fancybox="true") 
-savefig("senialesv_voz_2_bits.eps")
+savefig("senialesv_voz_2_bits_mu.eps")
 
--- Laboratorios/Lab1/Programas/Julia/teleco2.jl
+++ Laboratorios/Lab1/Programas/Julia/teleco2.jl
@@ -1,17 +1,17 @@
 # modulo para el problema 4 
 module Teleco2
 
-function set_sin(N::Number,fs::Number,f::Number)
+function set_sin(N::Int,fs::Number,f::Number)
     # funcion para generar una senial sinoidal
     # Inputs:
     # ------
-    # N: numero de muestras
-    # fs: frecuencia de muestreo 
-    # f: frecuencia de la senial 
+    # N: numero de muestras(numero entero)
+    # fs: frecuencia de muestreo(numero real) 
+    # f: frecuencia de la senial(numero real)
     # Outputs:
     # -------
-    # t: vector de tiepos
-    # x: senial generada
+    # t: vector de tiepos(Array)
+    # x: senial generada(Array)
 
     ts = 1/fs
     t = ts .* [0:N-1]
@@ -19,15 +19,16 @@ function set_sin(N::Number,fs::Number,f::Number)
 
     return x,t
 end
+
 function unifor_quatization(x::AbstractArray,N::Int)
     # funcion para cuantizar uniformente una senial
     # Inputs:
     # ------
-    # x: senial de entrada
-    # N: Niveles de cuantizacion
+    # x: senial de entrada(Array)
+    # N: niveles de cuantizacion(numero entero)
     # Output:
     # ------
-    # x_q: senial quantizada
+    # x_q: senial quantizada(Array)
 
     mag_max = maximum(abs(x))
     x_max = mag_max; x_min = -mag_max;
@@ -38,7 +39,30 @@ function unifor_quatization(x::AbstractArray,N::Int)
     return x_q
 end
 
+function μ_quantization(x::AbstractArray,N::Int,μ::Int)
+    # funcion para cuantizar mediante la ley-mu
+    # Inputs:
+    # ------
+    # x: senial de entrada(Array)
+    # N: niveles de cuantizacion(numero entero)
+    # μ: parametro(Numero real)
+    # Output:
+    # ------
+    # x_μ: senial cuantizada
 
+    mag_max = maximum(abs(x))
+    x_max = mag_max; x_min = -mag_max;
+    q = (x_max - x_min)/N;
+    
+    y = x_max*log10(1+abs(x)*(μ / x_max))/log10(1+μ);
+    
+    y_μ = floor((y-x_min)/q)*q+q/2+x_min;
+
+    x_μ = (x_max/μ)*(10.^((log10(1+μ)/x_max)*y_μ)-1).*sign(x);
+
+
+   return x_μ 
+end
 
 export unifor_quatization,set_sin
     
